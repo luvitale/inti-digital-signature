2ea1a3eb8c49912213968b4e0ac2155f720d454b
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2021-08-02 03:20:24 -0300

    Fixed test with languages

--- tests/unit/landing.spec.js
+++ tests/unit/landing.spec.js
@@ -1,11 +1,13 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
 import Landing from "@/components/Landing.vue";
+import i18n from "@/i18n";
 
 describe("Landing.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(Landing, {
+      i18n,
       propsData: { msg },
     });
     expect(wrapper.text()).to.include(msg);
